13880ba753cfab420a59e5ac91fe880dceb6d12e
David Sterry <sterry.david@gmail.com>
2021-06-03 14:33:24 -0700

    part two of tutorial, publish to surge

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style={{ color: "purple" }}>
-      <h1>Hello Internet!</h1>
+      <Link to="/contact">Contact</Link>
+      <Header headerText="Hello Internet!" />
       <p>What a great day</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
